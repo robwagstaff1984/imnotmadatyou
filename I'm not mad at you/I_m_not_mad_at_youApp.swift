e6541164815a23bf2eeab86346d32aeedf6944e1
Rob Wagstaff <robwagstaff1984@gmail.com>
2023-11-15 17:44:13 +1100

    setting up project

---
+++ I'm not mad at you/I_m_not_mad_at_youApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_m_not_mad_at_youApp.swift
+//  I'm not mad at you
+//
+//  Created by Rob on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct I_m_not_mad_at_youApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
