0869bfa2bd245caa44f82d73bfba4ca48001bc1c
Rob Wagstaff <robwagstaff1984@gmail.com>
2023-11-15 19:33:37 +1100

    add and read user data

--- I'm not mad at you/AppDelegate.swift
+++ I'm not mad at you/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import FirebaseDatabaseInternal
 import FirebaseCore
 import UserNotifications
 
@@ -7,6 +8,7 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Configure Firebase
         FirebaseApp.configure()
+        
 
         // Register for push notifications
         UNUserNotificationCenter.current().delegate = self
@@ -14,6 +16,34 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
         UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_, _ in })
         application.registerForRemoteNotifications()
 
+        var ref: DatabaseReference!
+
+        ref = Database.database(url: "https://im-not-mad-at-you-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
+
+        // Adding JohnDoe
+        ref.child("users").child("JohnDoe").setValue([
+            "lastTimeNotMad": "\(Date.now)",
+            "totalTimesNotMad": 5,
+            "isAdmin": false
+        ])
+
+        // Adding Steph
+        ref.child("users").child("Steph").setValue([
+            "isAdmin": true
+        ])
+
+        
+        //print
+        
+        ref.child("users").observeSingleEvent(of: .value, with: { snapshot in
+            if snapshot.exists() {
+                print("Users: \(snapshot.value!)")
+            } else {
+                print("No data available")
+            }
+        })
+
+        
         return true
     }
     
